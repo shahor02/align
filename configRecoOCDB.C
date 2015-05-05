f83c34877b6a85c4e95175019b4134e8fa6c4973
shahoian <ruben.shahoyan@cern.ch>
2015-05-05 22:55:04 +0200

    recoOCDB

---
+++ configRecoOCDB.C
@@ -0,0 +1,19 @@
+// this is a macro to setup the OCDB whose objects will be used
+// to undo the alignment/calibration applied during reconstruction
+
+void configRefOCDB(int run = 117220) 
+{
+  //
+  AliCDBManager* man = AliCDBManager::Instance();
+  man->ClearCache();
+  if (man->IsDefaultStorageSet()) man->UnsetDefaultStorage();
+  if (gSystem->AccessPathName("data/OCDB.root", kFileExists)==0) {        
+    man->SetDefaultStorage("local://$ALICE_ROOT/OCDB");    
+    man->SetRun(run);
+    man->SetSnapshotMode("data/OCDB.root");
+  }
+  else {
+    AliFatalGeneralF("","Failed to setup Reco-Time OCDB for run %d",run);
+  }
+  //
+}
