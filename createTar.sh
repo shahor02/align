4c29f96c11d299d845fe4f010e66b8baefb850f7
shahoian <ruben.shahoyan@cern.ch>
2015-05-08 14:26:48 +0200

    tar creator

---
+++ createTar.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+tar cvzf algGlo.tar.gz \
+AliAlgAux.cxx  AliAlgDetITS.cxx  AliAlgDetTPC.cxx  AliAlgMPRecord.cxx  \
+AliAlgRes.cxx   AliAlgSensITS.cxx  AliAlgSensTPC.cxx  AliAlgSteer.cxx  \
+AliAlgVol.cxx   AliAlgAux.h    AliAlgDetITS.h    AliAlgDetTPC.h    AliAlgMPRecord.h \
+AliAlgRes.h     AliAlgSensITS.h    AliAlgSensTPC.h    AliAlgSteer.h    AliAlgVol.h \
+AliAlgDet.cxx  AliAlgDetTOF.cxx  AliAlgDetTRD.cxx  AliAlgPoint.cxx     AliAlgSens.cxx  \
+AliAlgSensTOF.cxx  AliAlgSensTRD.cxx  AliAlgTrack.cxx  AliAlgVtx.cxx \
+AliAlgDet.h    AliAlgDetTOF.h    AliAlgDetTRD.h    AliAlgPoint.h  \
+AliAlgSens.h    AliAlgSensTOF.h    AliAlgSensTRD.h    AliAlgTrack.h    AliAlgVtx.h \
+Makefile
+
